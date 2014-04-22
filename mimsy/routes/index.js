596e8659ccf3c2b3928aaf11d86b0c9d118a6244
Caleb Cauthon <calebcauthon@gmail.com>
2014-04-22 13:29:16 -0500

    Added the create page.

--- mimsy/routes/index.js
+++ mimsy/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mimsy MMS' });
+});
+
+router.get('/create', function(req, res) {
+  res.render('create', { title: 'Mimsy MMS - Create Slideshow' });
 });
 
 module.exports = router;
