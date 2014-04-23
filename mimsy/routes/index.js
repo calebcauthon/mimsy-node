f245d4beab4531c674c4297a959cc1fb07b925be
Caleb Cauthon <calebcauthon@gmail.com>
2014-04-23 15:43:13 -0500

    Basic create/view slideshow.

--- mimsy/routes/index.js
+++ mimsy/routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var uuid = require('node-uuid');
+require('locus');
+
+var redis = require('redis');
+var db = redis.createClient();
+
+db.on("error", function (err) {
+  console.log("Error " + err);
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -7,7 +16,55 @@ router.get('/', function(req, res) {
 });
 
 router.get('/create', function(req, res) {
-  res.render('create', { title: 'Mimsy MMS - Create Slideshow' });
+  res.render('create', { phone_number: "", pictures: [], uuid: uuid.v1(), title: 'Mimsy MMS - Create Slideshow' });
+});
+
+router.get('/pictures/:uuid', function(req, res) {
+  db.hgetall(req.params.uuid, function(err, reply) {
+    var phone_number = reply && reply.phone_number;
+    db.smembers("pictures_" + req.params.uuid, function(err, reply) {
+      res.render('text', { text: reply });
+    });
+  });
+});
+
+router.get('/slideshow/:uuid', function(req, res) {
+  db.hgetall(req.params.uuid, function(err, reply) {
+    var phone_number = reply && reply.phone_number;
+    db.smembers("pictures_" + req.params.uuid, function(err, reply) {
+      res.render('create', { phone_number: phone_number, pictures: reply, uuid: req.params.uuid, title: 'Mimsy MMS - Slideshow' });
+    });
+  });
+});
+
+router.post('/slideshow/save', function(req, res) {
+  var slideshow_uuid = req.body.slideshow_uuid;
+  var phone_number = req.body.phone_number;
+
+  db.hmset(slideshow_uuid, { phone_number: phone_number } , function() {});
+  db.set(phone_number, slideshow_uuid, function() {});
+
+  res.render('index', { title: 'Mimsy MMS' });
+});
+
+router.post('/receive_mms', function(req, res) {
+  var image_url = req.body.images[0].image;
+  var phone_number = req.body.msisdn;
+  
+  db.get(phone_number, function(err, slideshow_uuid) {
+    db.sadd("pictures_" + slideshow_uuid, image_url, function() {});  
+    res.render('text', { text: '' });
+  });
+});
+
+router.get('/:digits.jpg', function(req, res) {
+  var phone_number = req.params.digits;
+  
+  db.get("picture-1-" + phone_number, function(err, reply) {
+    var image_url = reply;    
+
+    res.render('image', { phone_number: phone_number, image_url: image_url, title: 'Mimsy MMS' });  
+  });
 });
 
 module.exports = router;
